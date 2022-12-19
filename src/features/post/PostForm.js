600497d40ffb143526df840e3c34f7257ced32ef
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-12-19 16:54:24 +0700

    finish logic create new post with UI design

--- src/features/post/PostForm.js
+++ src/features/post/PostForm.js
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Box, Card, alpha, Stack } from "@mui/material";
+
+import { FormProvider, FTextField } from "../../components/form";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import { LoadingButton } from "@mui/lab";
+import { createPost } from "./postSlice";
+
+const PostSchema = Yup.object().shape({
+  content: Yup.string().required("Content is required"),
+});
+
+const defaultValues = {
+  content: "",
+  image: "",
+};
 
 function PostForm() {
-  return <div>PostForm</div>;
+  const { isLoading } = useSelector((state) => state.post);
+  const methods = useForm({
+    resolver: yupResolver(PostSchema),
+    defaultValues,
+  });
+  const {
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { isSubmitting },
+  } = methods;
+
+  const dispatch = useDispatch();
+  const onSubmit = async (data) => {
+    dispatch(createPost(data)).then(() => reset());
+  };
+
+  const fileInput = useRef();
+  const handleFile = (e) => {
+    const file = fileInput.current.files[0];
+    if (file) {
+      setValue("image", file);
+    }
+  };
+  return (
+    <Card sx={{ p: 3 }}>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2}>
+          <FTextField
+            name="content"
+            multiline
+            fullWidth
+            rows={4}
+            placeholder="Share what you are thinking here..."
+            sx={{
+              "& fieldset": {
+                borderWidth: `1px !important`,
+                borderColor: alpha("#919EAB", 0.32),
+              },
+            }}
+          />
+
+          {/* <FTextField name="Image" placeholder="Image" /> */}
+
+          <input type="file" ref={fileInput} onChange={handleFile} />
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              allignItem: "center",
+            }}
+          >
+            <LoadingButton
+              type="submit"
+              variant="contained"
+              size="small"
+              loading={isSubmitting || isLoading}
+            >
+              Post
+            </LoadingButton>
+          </Box>
+        </Stack>
+      </FormProvider>
+    </Card>
+  );
 }
 
 export default PostForm;
--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import apiService from "../../app/apiService";
 
 const initialState = {
   isLoading: false,
@@ -8,7 +9,32 @@ const initialState = {
 const slice = createSlice({
   name: "post",
   initialState,
-  reducers: {},
+  reducers: {
+    startLoading(state) {
+      state.isLoading = true;
+    },
+    hasError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    createPostSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.post = action.payload.posts;
+    },
+  },
 });
+
+export const createPost =
+  ({ content, image }) =>
+  async (dispatch) => {
+    dispatch(slice.actions.startLoading());
+    try {
+      const response = await apiService.post("/posts", { content, image });
+      dispatch(slice.actions.createPostSuccess(response.data));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error.message));
+    }
+  };
 
 export default slice.reducer;
