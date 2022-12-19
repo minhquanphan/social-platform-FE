fcfae8f179046fa901d0371da9e108336408a3d5
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-12-19 15:11:51 +0700

    profile UI design

--- src/features/user/Profile.js
+++ src/features/user/Profile.js
@@ -1,7 +1,33 @@
+import { Grid, Stack } from "@mui/material";
 import React from "react";
+import useAuth from "../../hooks/useAuth";
+import ProfileAbout from "./ProfileAbout";
+import ProfileScorecard from "./ProfileScorecard";
+import ProfileSocialInfo from "./ProfileSocialInfo";
+import PostList from "../post/PostList";
+import PostForm from "../post/PostForm";
 
-function Profile() {
-  return <div>Profile</div>;
+function Profile({ profile }) {
+  const { user } = useAuth();
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={4}>
+        <Stack spacing={3}>
+          <ProfileScorecard profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileSocialInfo profile={profile} />
+        </Stack>
+      </Grid>
+
+      <Grid item xs={12} md={8}>
+        <Stack spacing={3}>
+          {user._id === profile._id && <PostForm />}
+          <PostList userId={profile._id} />
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Profile;
