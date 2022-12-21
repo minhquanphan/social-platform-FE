d827725f0e48679edc86c65537fad9cd5e0c2c04
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-12-21 10:42:22 +0700

    comment form UI design

--- src/features/comment/CommentForm.js
+++ src/features/comment/CommentForm.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CommentForm() {
-  return <div>CommentForm</div>;
+import { Stack, Avatar, TextField, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+import useAuth from "../../hooks/useAuth";
+
+function CommentForm({ postId }) {
+  const { user } = useAuth();
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(content);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Stack direction="row" alignItems="center">
+        <Avatar src={user.avatarUrl} alt={user.name} />
+        <TextField
+          fullWidth
+          size="small"
+          value={content}
+          placeholder="Write a comment…"
+          onChange={(event) => setContent(event.target.value)}
+          sx={{
+            ml: 2,
+            mr: 1,
+            "& fieldset": {
+              borderWidth: `1px !important`,
+              borderColor: (theme) =>
+                `${theme.palette.grey[500_32]} !important`,
+            },
+          }}
+        />
+        <IconButton type="submit">
+          <SendIcon sx={{ fontSize: 30 }} />
+        </IconButton>
+      </Stack>
+    </form>
+  );
 }
 
 export default CommentForm;
