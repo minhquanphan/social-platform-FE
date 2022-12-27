bde53d726bcb64f970304ec318ad150b69f8dfdd
minhquanphan <57800230+minhquanphan@users.noreply.github.com>
2022-12-27 16:31:03 +0700

    account page incomplete UI design

---
+++ src/features/user/AccountSocialLink.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AccountSocialLink() {
+  return <div>AccountSocialLink</div>;
+}
+
+export default AccountSocialLink;
--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,7 +1,58 @@
-import React from "react";
+import { Box, Container, Tab, Tabs, Typography } from "@mui/material";
+import React, { useState } from "react";
+import AccountGeneral from "../features/user/AccountGeneral";
+import AccountSocialLink from "../features/user/AccountSocialLink";
+import useAuth from "../hooks/useAuth";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ShareIcon from "@mui/icons-material/Share";
+import { capitalCase } from "change-case";
 
 function AccountPage() {
-  return <div>AccountPage</div>;
+  const { user } = useAuth();
+  const [currentTab, setCurrentTab] = useState("general");
+
+  const handleChangeTab = (newValue) => {
+    setCurrentTab(newValue);
+  };
+
+  const ACCOUNT_TABS = [
+    {
+      value: "general",
+      icon: <AccountBoxIcon sx={{ fontSize: 30 }} />,
+      component: <AccountGeneral />,
+    },
+    {
+      value: "social_link",
+      icon: <ShareIcon sx={{ fontSize: 30 }} />,
+      component: <AccountSocialLink />,
+    },
+  ];
+  return (
+    <Container>
+      <Typography>Account Page</Typography>
+      <Tabs
+        value={currentTab}
+        onChange={(e, value) => handleChangeTab(value)}
+        variant="scrollable"
+        scrollButtons="auto"
+        allowScrollButtonsMobile
+      >
+        {ACCOUNT_TABS.map((tab) => (
+          <Tab
+            disableRipple
+            key={tab.value}
+            value={tab.value}
+            icon={tab.icon}
+            label={capitalCase(tab.value)}
+          />
+        ))}
+      </Tabs>
+      {ACCOUNT_TABS.map((tab) => {
+        const isMatch = tab.value === currentTab;
+        return isMatch && <Box key={tab.value}>{tab.component}</Box>;
+      })}
+    </Container>
+  );
 }
 
 export default AccountPage;
